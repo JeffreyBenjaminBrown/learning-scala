a427e73f9c28869e44d93b908f4aa7330d2ae9ff
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-25 18:13:26 -0500

    org

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,4 +1,5 @@
-# When files are generated in the Docker container, run this.
+# When files are generated in the Docker container,
+# run this to gain access to them from outside of Docker.
 
 sudo chown jeff -R .
 sudo find . -type f -exec chmod 0644 {} \;
